9e16d0a53edc0af4f24892430331ddcd7a1f4ac1
Koreanerd <koreanerd@Koreanerds-MacBook-Air.local>
2023-11-22 00:35:53 +0900

    feat: Implement enhanced session management, error handling, and server address form
    - Revamped routing and authentication logic in App.jsx, SignIn.jsx, and ServerAddressList.jsx.
    - Addressed session persistence issues, ensuring seamless user experience across browser refreshes using onAuthStateChanged and local storage.
    - Enhanced API error handling in signInWithGoogle, authenticateUser, and getUserInformation functions for improved robustness.
    - Applied early return pattern in React components for better readability and efficiency.
    - Developed new ServerAddressForm component to enable user-friendly server address registration.
      - Implemented form handling with Redux state management for user ID and token.
      - Added error handling and user feedback within the form submission process.
    - General code optimizations and refactoring for better maintainability and performance.
    
    This commit introduces crucial improvements in session management and error handling, alongside adding a new feature for server address management in the CoreCity project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Welcome from "./views/Welcome/Welcome";
-import ServerList from "./views/ServerAddressList";
+import Welcome from "./views/welcome/Welcome";
+import ServerList from "./views/server-list/ServerAddressList";
 import Error from "./views/Error";
 import "./App.css";
+import { getAuth } from "firebase/auth";
+import app from "./config/firebaseConfig";
+import { useDispatch } from "react-redux";
+import { clearUser, setUser } from "./features/user/userSlice";
+import { useEffect } from "react";
+import { getUserInformation } from "./api/api";
 
 function App() {
+  const auth = getAuth(app);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (!user) {
+        dispatch(clearUser());
+
+        return;
+      }
+
+      const token = await user.getIdToken();
+      const result = await getUserInformation(user.uid, token);
+
+      if (result.error) {
+        console.error(result.error);
+
+        return;
+      }
+
+      dispatch(
+        setUser({
+          id: result._id,
+          email: result.email,
+          name: result.name,
+          uid: result.uid,
+          token: token,
+        }),
+      );
+    });
+
+    return () => unsubscribe();
+  }, [dispatch]);
+
   return (
     <Router>
       <Routes>
---
+++ src/views/server-list/ServerAddressForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { addServerAddress } from "../../api/api";
+import PropTypes from "prop-types";
+
+function ServerAddressForm({ onAddAddress }) {
+  const userId = useSelector((state) => state.user.id);
+  const token = useSelector((state) => state.user.token);
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!address) return;
+
+    const result = await addServerAddress(userId, token, address);
+
+    if (result.error) {
+      alert(result.error);
+
+      return;
+    }
+
+    setAddress("");
+    onAddAddress();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input type="submit" value={"Add"} />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+ServerAddressForm.propTypes = {
+  onAddAddress: PropTypes.func.isRequired,
+};
+
+export default ServerAddressForm;
