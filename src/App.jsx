ba635b8e7278fe1c7e224e51dae268f700bc60ae
Koreanerd <koreanerd@Koreanerds-MacBook-Air.local>
2024-01-28 01:21:01 +0900

    chore: Edit directories and component names
    - welcome/Welcome.jsx -> landing-page/LandingPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "@/views/header/Header";
-import Welcome from "@/views/welcome/Welcome";
+import LandingPage from "@/views/landing-page/LandingPage";
 import ServerList from "@/views/server-list/ServerAddressList";
 import Dashboard from "@/views/dashboard/Dashboard";
 import Error from "@/views/Error";
@@ -9,14 +9,13 @@ import useLoadApprovalRequestServerList from "@/hooks/useLoadApprovalRequestServ
 
 function App() {
   useAuthState();
-
   useLoadApprovalRequestServerList();
 
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/server-addresses" element={<ServerList />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/error" element={<Error />} />
