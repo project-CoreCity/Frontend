e9d182b3060424065015c3d058da18982d3fe83d
Koreanerd <koreanerd@Koreanerds-MacBook-Air.local>
2024-03-11 01:42:04 +0900

    chore: Apply HTML Semantic Elements
    - Implemented main tags in parent components for each route to clarify document structure
    - Reduced indiscriminate use of div tags, replacing them with semantically appropriate HTML tags to improve accessibility and SEO

--- src/views/header/Header.jsx
+++ src/views/header/Header.jsx
@@ -14,42 +14,44 @@ function Header() {
     location.pathname === "/" ? "bg-[#0000BA]" : "bg-[#293137]";
 
   return (
-    <div
-      className={`flex items-center justify-between h-16 ${backgroundColor}`}
-    >
-      <Link to="/">
-        <h1 className="mx-3 font-figtree text-white text-3xl">Core City</h1>
-      </Link>
-      {isAuthenticated === null ? (
-        ""
-      ) : isAuthenticated ? (
-        <UserMenu />
-      ) : (
-        <div className="flex">
-          {currentPath === "guestMode" ? (
-            <NavigationButton
-              path={"/"}
-              title="Back to Home"
-              css="flex items-center justify-center px-3 py-2 rounded-md bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-300 hover:to-cyan-300 font-medium text-[#0000BA]"
-              ariaLabel="Server list view button"
-            />
-          ) : (
-            <DashboardButton
-              address="guestMode"
-              text="Guest Mode"
-              css="flex items-center justify-center px-3 py-2 rounded-md bg-gradient-to-r from-green-300 to-cyan-500 hover:from-blue-300 hover:to-cyan-300 font-medium text-[#0000BA]"
-            />
-          )}
+    <header>
+      <nav
+        className={`flex items-center justify-between h-16 ${backgroundColor}`}
+      >
+        <Link to="/">
+          <h1 className="mx-3 font-figtree text-white text-3xl">Core City</h1>
+        </Link>
+        {isAuthenticated === null ? (
+          ""
+        ) : isAuthenticated ? (
+          <UserMenu />
+        ) : (
+          <div className="flex">
+            {currentPath === "guestMode" ? (
+              <NavigationButton
+                path={"/"}
+                title="Back to Home"
+                css="flex items-center justify-center px-3 py-2 rounded-md bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-300 hover:to-cyan-300 font-medium text-[#0000BA]"
+                ariaLabel="Server list view button"
+              />
+            ) : (
+              <DashboardButton
+                address="guestMode"
+                text="Guest Mode"
+                css="flex items-center justify-center px-3 py-2 rounded-md bg-gradient-to-r from-green-300 to-cyan-500 hover:from-blue-300 hover:to-cyan-300 font-medium text-[#0000BA]"
+              />
+            )}
 
-          <AuthenticationButton
-            handler={handleSignIn}
-            title="Sign in"
-            css="mx-3 w-28 h-10 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-300 text-md text-[#0000BA]"
-            ariaLabel="Sign in button"
-          />
-        </div>
-      )}
-    </div>
+            <AuthenticationButton
+              handler={handleSignIn}
+              title="Sign in"
+              css="mx-3 w-28 h-10 rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-300 hover:to-blue-300 text-md text-[#0000BA]"
+              ariaLabel="Sign in button"
+            />
+          </div>
+        )}
+      </nav>
+    </header>
   );
 }
 
